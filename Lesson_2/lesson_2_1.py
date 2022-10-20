4bb1ef85cbdbdad45bb413aac983ce01399b493f
sibirkoda <denis.a.sibirko@mail.ru>
2022-10-20 16:04:10 +0300

    added lesson 2

---
+++ Lesson_2/lesson_2_1.py
@@ -0,0 +1,33 @@
+colors = ['red', 'green', 'blue']
+# data = open('file.txt', 'w')
+# data.writelines(colors)
+# data.write('\n LINE 2')
+# data.write('\n LINE 3')
+# data.close()
+
+# with open ('file.txt', 'a') as data:
+#     data.write(' text1 \n')
+#     data.write(' text2 \n')
+
+# path = 'file.txt'
+# data = open(path, 'r')
+# for line in data:
+#     print(line)
+# data.close()
+
+# def f(x):
+#     if x ==1:
+#         return 'Целое'
+#     elif x == 2.3:
+#         return 23
+#     else:
+#         return
+# print(f(1))
+
+# def new_string (symbol, count):
+#     return symbol * count
+# print (new_string ('!',5))
+
+a = (3,4)
+print (a)
+print (a[0])
