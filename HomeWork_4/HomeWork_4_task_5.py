4329b2f8125f1dc8364679a50f2575c2ca070e0d
sibirkoda <denis.a.sibirko@mail.ru>
2022-10-26 15:12:52 +0300

    continue added HomeWork_4_task_5.py

--- HomeWork_4/HomeWork_4_task_5.py
+++ HomeWork_4/HomeWork_4_task_5.py
@@ -2,11 +2,10 @@
 Даны два файла, в каждом из которых находится запись многочлена. 
 Задача - сформировать файл, содержащий сумму многочленов.
 """
-
+"""
 import random
+
 # функция-генератор знака у элемента многочлена
-
-
 def znak():
     if random.randint(0, 2) == 0:
         a = 1
@@ -15,8 +14,6 @@ def znak():
     return a
 
 # функция создает многочлен
-
-
 def create(k):
     s1 = []
     for i in range(0, k+1):
@@ -53,3 +50,64 @@ with open('file6.txt', 'r') as file:
     s2 = file.readline()
 print(s1)
 print(s2)
+
+"""
+s1 = '94*x^3-51*x^2+82*x^1+19=0'
+temp_s1 = s1.replace('=0', '')
+
+# Функция формирует список из индексов символов "+" и "-" в строке
+def IndexZnak(s):
+    last = len(s)
+    terms = []
+    first = 1
+    while (first < last):
+        l = s.find('-', first, last)
+        if l == -1:
+            break
+        terms.append(l)
+        first = l+1
+    first = 0
+    while (first < last):
+        l = s.find('+', first, last)
+        if l == -1:
+            break
+        terms.append(l)
+        first = l+1
+    return terms
+
+# Функция для разбития многочлена на слагаемые и формирование из них списка
+def Terms(s):
+    j = 0
+    terms = []
+    for i in index1:
+        terms.append(temp_s1[j:i])
+        j = i
+    terms.append(temp_s1[index1[-1]:])
+    for item in terms:
+        i=0
+        if item[0] != '+':
+            if item[0] != '-':
+                terms[i] = '+' + item
+        i+=1
+    for item in terms:
+        i=0
+        if item.find('*x^') == -1:
+            terms[i] = item +'*x^0'
+        i+=1
+    
+    return terms
+
+print (s1)
+index1 = IndexZnak(temp_s1)
+terms1 = Terms(index1)
+
+print(index1)
+print(terms1)
+
+
+
+cor1 = []
+for item in terms1:
+    cor1.append(item.split('*x^'))
+
+print(cor1)
